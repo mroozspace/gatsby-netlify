2d8158155155dbb765d813c6003eceba2a93a902
adrian mroz <adrian.mroz@besidethepark.com>
2019-01-28 13:24:08 +0100

    content added

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,6 +12,9 @@ const IndexPage = () => (
     <p>Welcome to your new Gatsby site.</p>
     <p> paragraph added </p>
     <p> -- one more paragraph added </p>
+    <p> -- one more paragraph added </p>
+    <p> -- one more paragraph added </p>
+    <p> -- one more paragraph added </p>
     <p>Now go build something great.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
